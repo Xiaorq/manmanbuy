64eab21e806a6df01e9bb11e7e5b49b6f3327246
WinfredH <hz873001860@live.cn>
2018-03-25 17:08:01 +0800

    brand.html完成，brand-list.html新建

--- js/brand.js
+++ js/brand.js
@@ -1,3 +1,31 @@
 /**
  * Created by hz on 2018/3/25.
  */
+
+$(() => {
+    const brand = new Brand();
+});
+
+const Brand = (() => {
+    function Constructor() {
+        this.init();
+    }
+    Constructor.prototype = {
+        init: function () {
+            mui('.mui-scroll-wrapper').scroll({
+                deceleration: 0.0005,
+                indicators: false,
+            });
+            $.ajax({
+                url: 'http://mmb.ittun.com/api/getbrandtitle',
+                dataType: 'json',
+                success: data => {
+                    // render
+                    const element = template('brandCategoryTemp',data);
+                    $('.brand-list').append(element);
+                }
+            });
+        },
+    };
+    return Constructor;
+})();
